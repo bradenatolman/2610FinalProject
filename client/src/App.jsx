170e08d2bc126f42dcd5bd1d57219efa1856d544
bradenatolman <bradenatolman@gmail.com>
2025-11-30 01:02:35 -0700

    updated base components-branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { TableView } from './tableView.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   async function logout() {
     const res = await fetch("/registration/logout/", {
@@ -21,27 +19,13 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="Navbar">
+       <button onClick={logout}>Logout</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="Page">
+        <TableView />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <button onClick={logout}>Logout</button>
+      
     </>
   )
 }
