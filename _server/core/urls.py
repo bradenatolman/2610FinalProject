7acce81cd4a8a4e02846110d498006e4a8c699f8
bradenatolman <bradenatolman@gmail.com>
2025-12-02 13:52:28 -0700

    combined enter-purchase urls.py

--- _server/core/urls.py
+++ _server/core/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', view=views.index, name="index"),
-    path("tableInfo/<int:year>/<int:month>/", view=views.tableInfo, name="tableInfo")
+    path("tableInfo/<int:year>/<int:month>/", view=views.tableInfo, name="tableInfo"),
+    path("categories/", view=views.categories, name="categories"),
+    path("purchases/", view=views.purchases, name="purchases")
 ]
