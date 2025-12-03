97d5cfeea4feaad3dba699b1824bd7a167c6c17d
Dallin Matthew Moon <A02338740@usu.edu>
2025-12-03 10:54:51 -0700

    patched from budget tables again

--- client/src/tableView.jsx
+++ client/src/tableView.jsx
@@ -1,31 +1,13 @@
 import "./tableView.css";
 import { useEffect, useState } from "react";
 
-export function TableView() {
+export function TableView(props) {
+    const { edit } = props;
     const [monthName, setMonthName] = useState("");
     const [month, setMonth] = useState(0);
     const [year, setYear] = useState(0);
     const [categories, setCats] = useState([])
     const [subcategories, setSubs] = useState([])
-
-
-    async function getTableInfo() {
-        const res = await fetch(`/tableInfo/${year}/${month}/`, {
-        credentials: "same-origin",
-        })
-
-        const body = await res.json();
-        setMonth(body.month);
-        setYear(body.year);
-        setMonthName(body.monthName);
-        setCats(body.categories);
-        setSubs(body.subcategories);
-    }
-
-    useEffect(() => {
-        console.log("Fetching table info for", month, year);
-        getTableInfo();
-    }, [month])
 
     function updateMonth(delta) {
         return () => {
@@ -43,21 +25,48 @@ export function TableView() {
         }
     }
 
+    async function getTableInfo() {
+        const res = await fetch(`/tableInfo/${year}/${month}/`, {
+        credentials: "same-origin",
+        })
+
+        const body = await res.json();
+        setMonth(body.month);
+        setYear(body.year);
+        setMonthName(body.monthName);
+        setCats(body.categories);
+        setSubs(body.subcategories);
+
+        console.log(body);
+    }
+
+    // Changes Month and displays data for that month
+    useEffect(() => {
+        getTableInfo();
+    }, [month])
+
+    // Updates when edited
+    useEffect(() => {
+        
+    }, [categories, subcategories])
+
+
     return (
          <div>  
             <div className="title-month">
                 <h1> 
                     <span onClick={updateMonth(-1)}> 〈 </span>
                     {monthName} {year}
-                    <span onClick={updateMonth(1)}> 〉</span></h1>
+                    <span onClick={updateMonth(1)}> 〉</span>
+                </h1>
             </div>
-            <div className="categories-grid">
+
+            <div className="table-container">
                 {categories.map(cat => {
-                    const subsForCat = subcategories.filter(s => s.categoryId === cat.id);
+                    const subsForCat = subcategories.filter(s => s.category === cat.id);
                     return (
-                        <div key={cat.id} className="category-cell">
-                            {subsForCat.length ? (
-                                <table>
+                        <div key={cat.id} className="category-table">
+                            <table>
                                     <thead>
                                         <tr>
                                             <th>{cat.name}</th>
@@ -65,18 +74,21 @@ export function TableView() {
                                             <th>Actual</th>
                                         </tr>
                                     </thead>
-                                    <tbody>
-                                        {subsForCat.map(sub => (
-                                            <tr key={sub.id}>
-                                                <td>{sub.name}</td>
-                                                <td>{sub.amount ?? '-'}</td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-                            ) : (
-                                <button>Add Subcategory</button>
-                            )}
+                                    {subsForCat.length ? (
+                                            <tbody>
+                                                {subsForCat.map(sub => (
+                                                    <tr key={sub.id}>
+                                                        <td>{sub.name}</td>
+                                                        <td>{sub.amount ?? '-'}</td>
+                                                        <td>{sub.actual ?? '-'}</td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                    
+                                    ) : (
+                                        <tbody><tr><td>{edit ?(<button>Add Subcategory</button>) : '-'}</td><td>-</td><td>-</td></tr></tbody>
+                                    )}
+                             </table>
                         </div>
                     );
                 })}
